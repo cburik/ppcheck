04cc570585a008d12b8cd93bda14f66600559786
Casper Burik <cburik@users.noreply.github.com>
2024-03-02 10:25:08 +0100

    Introduces pathlib

--- pwned_password_checker/constants.py
+++ pwned_password_checker/constants.py
@@ -1,2 +1,4 @@
-DB_LOC = "database/database.db"
+from pathlib import Path
+
+DB_LOC = Path.home() / ".pwned_password_checker" / "pwned_passwords.db"
 API_URL = "https://api.pwnedpasswords.com/range"
