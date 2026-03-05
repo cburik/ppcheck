1e59d55193140f5afbe9d17a98758fe307cc03f8
Casper Burik <cburik@users.noreply.github.com>
2026-03-05 23:18:16 +0100

    encryption bb

---
+++ ppcheck/encryption.py
@@ -0,0 +1,57 @@
+import base64
+import getpass
+import os
+from functools import lru_cache
+
+from cryptography.fernet import Fernet, InvalidToken
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+
+
+class EncryptionManager:
+    _instance = None
+    _password = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    @staticmethod
+    def generate_salt() -> bytes:
+        return os.urandom(16)
+
+    def encrypt(self, data: bytes, salt: bytes) -> bytes:
+        if self._password is None:
+            self._get_password()
+        fernet = self._initialize_fernet(salt)
+        return fernet.encrypt(data)
+
+    def encrypt_str(self, data: str, salt: bytes) -> bytes:
+        return self.encrypt(data.encode(), salt)
+
+    def decrypt(self, data: bytes, salt: bytes) -> bytes:
+        if self._password is None:
+            self._get_password()
+        try:
+            fernet = self._initialize_fernet(salt)
+            return fernet.decrypt(data)
+        except InvalidToken:
+            raise ValueError("Invalid token")
+
+    def decrypt_str(self, data: bytes, salt: bytes) -> str:
+        return self.decrypt(data, salt).decode()
+
+    def _get_password(self) -> None:
+        self._password = getpass.getpass("Enter encryption password: ").encode()
+
+    @lru_cache(maxsize=1024)
+    def _initialize_fernet(self, salt):
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=salt,
+            iterations=50_000,  # TODO: encrease after developing (and set with env var for testing)
+        )
+        key = base64.urlsafe_b64encode(kdf.derive(self._password))
+        return Fernet(key)
--- tests/conftest.py
+++ tests/conftest.py
@@ -2,10 +2,17 @@ from pytest import fixture
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
+from ppcheck.encryption import EncryptionManager
 from ppcheck.install import create_database
 from ppcheck.utils import remove_file
 
 TMP_DB_LOC = "./tmp_database.db"
+
+
+@fixture(scope="session", autouse=True)
+def encryption_manager_with_test_password():
+    manager = EncryptionManager()
+    manager._password = b"test-password"
 
 
 @fixture
